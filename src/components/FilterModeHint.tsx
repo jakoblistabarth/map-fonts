11945ca734037f98b39bb6174d2be843a9c92011
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2026-09-03 09:56:20 +0200

    :lipstick: Use card component for hint

--- src/components/FilterModeHint.tsx
+++ src/components/FilterModeHint.tsx
@@ -1,25 +1,32 @@
 import { withBase } from "@/utils/base";
-import { RulerDimensionLineIcon } from "lucide-react";
+import { ArrowBigRightDashIcon, RulerDimensionLineIcon } from "lucide-react";
 import type { FC } from "react";
+import { Button } from "./ui/button";
+import { Card, CardContent } from "./ui/card";
 
 export const FilterModeHint: FC = () => {
   return (
-    <div className="rounded-4xl border border-indigo-400 bg-indigo-50 p-5">
-      <div className="mb-5 flex aspect-square size-12 items-center justify-center rounded-full border border-indigo-200">
-        <RulerDimensionLineIcon className="stroke-indigo-400" />
-      </div>
-      <h2 className="mb-5 text-lg font-bold text-indigo-500">
-        Not yet what you were looking for?
-      </h2>
-      <p>
-        If you have a specific mood in mind or you want to find a font based on
-        its metrics, try the{" "}
-        <a className="underline" href={withBase("expert-mode")}>
-          Filter Mode
-        </a>{" "}
-        to find the perfect font for your map.
-      </p>
-    </div>
+    <Card className="">
+      <CardContent className="space-y-5">
+        <div className="flex aspect-square size-12 items-center justify-center rounded-full border border-indigo-200">
+          <RulerDimensionLineIcon className="stroke-indigo-400" />
+        </div>
+        <h2 className="mb-5 text-lg font-bold text-indigo-500">
+          Not yet what you were looking for?
+        </h2>
+        <p>
+          If you have a specific mood in mind or you want to find a font based
+          on its metrics, try the <em>Filter Mode</em> to find the perfect font
+          for your map.
+        </p>
+        <a href={withBase("expert-mode")}>
+          <Button className="flex gap-2">
+            <ArrowBigRightDashIcon />
+            <span>Go to Filter Mode</span>
+          </Button>
+        </a>
+      </CardContent>
+    </Card>
   );
 };
 
